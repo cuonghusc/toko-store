e554a3489c65d7c433037d37d052d95598e31ef2
cuonghusc <cuonghusc@gmail.com>
2020-02-22 11:46:22 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,21 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('{username}/profile',function($username){
+    echo $username;
+});
+
+Route::prefix('user')->group(function(){
+    Route::get('',function(){
+        return view('user');
+    });
+    Route::get('{id}', function ($id) {
+        echo $id;
+    })->where('id', '[0-9]+');
+});
+//validate parameter only number
+
+Route::get('search/{search}', function ($search) {
+    echo $search;
+})->where('search', '.*');
